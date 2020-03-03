da6c0ff01346b66dc18ad44921db507aad42922d
Sergei Alexeev <serenehet@gmail.com>
2020-03-03 16:41:53 +0300

    save changes

---
+++ iz1/cmake-build-debug/CMakeFiles/iz1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/iz1.dir/dz.c.o"
+  "CMakeFiles/iz1.dir/main.c.o"
+  "iz1"
+  "iz1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/iz1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
