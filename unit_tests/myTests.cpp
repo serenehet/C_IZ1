1182a73a2db9f4a00e5edf2c009601805ae9c39e
Sergei Alexeev <serenehet@gmail.com>
2020-03-08 14:38:23 +0300

    Удаление некоректных тестов

--- unit_tests/myTests.cpp
+++ unit_tests/myTests.cpp
@@ -13,39 +13,8 @@ TEST(testAreaComparison, test1) {
     ASSERT_EQ(area1 > area2, true);
 }
 
+
 TEST(testDecision, test1) {
-    size_t size = 3;
-    float ** xArray = (float **)calloc(3, sizeof(float *));
-    float ** yArray = (float **)calloc(3, sizeof(float *));
-    int ** res = (int **)calloc(3, sizeof(int *));
-    for (int i = 0; i < size; ++i) {
-        xArray[i] = (float *)malloc(sizeof(float));
-        yArray[i] = (float *)malloc(sizeof(float));
-        res[i] = (int *)malloc(sizeof(int));
-    }
-
-    xArray[0][0] = 0; yArray[0][0] = 0;
-    xArray[1][0] = 0; yArray[1][0] = 9;
-    xArray[2][0] = 9; yArray[2][0] = 0;
-
-    res[0][0] = 0; res[1][0] = 1; res[2][0] = 2;
-
-    int ** testRes = getIndexOfMaxAreaTriangle(xArray, yArray, size);
-    ASSERT_EQ(res[0][0], testRes[0][0]);
-    ASSERT_EQ(res[1][0], testRes[1][0]);
-    ASSERT_EQ(res[2][0], testRes[2][0]);
-
-    freeIndexArray(&testRes, 3);
-    freeIndexArray(&res, 3);
-    float ** pointsArray = (float **)calloc(2, sizeof(float *));
-    pointsArray[0] = *xArray;
-    pointsArray[1] = *yArray;
-    freePointsArray(&pointsArray, 2);
-    free(xArray);
-    free(yArray);
-}
-
-TEST(testDecision, test2) {
     size_t size = 0;
     float **xArray = NULL;
     float **yArray = NULL;
@@ -55,7 +24,7 @@ TEST(testDecision, test2) {
 
 }
 
-TEST(testDecision, test3) {
+TEST(testDecision, test2) {
     size_t size = 2;
     float **xArray = NULL;
     float **yArray = NULL;
@@ -65,7 +34,7 @@ TEST(testDecision, test3) {
 }
 
 
-TEST(testDecision, test4) {
+TEST(testDecision, test3) {
     size_t size = 4;
     float ** pointsArray = (float **)calloc(2, sizeof(float *));
     pointsArray[0] = (float *)calloc(size, sizeof(float));
